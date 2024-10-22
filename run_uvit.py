969fdb5393bb515864464fe4829069897c7d6551
BrandenKeck <bmkeck62@gmail.com>
2024-10-22 22:02:03 +0000

    reinit

---
+++ run_uvit.py
@@ -0,0 +1,102 @@
+# Imported Libraries
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision.utils import save_image
+from torch.optim.lr_scheduler import LambdaLR
+from torchvision.transforms import ToTensor, Compose, CenterCrop, Normalize
+
+
+# Imported Model(s)
+import uvit.sde as sde
+from uvit.uvit import UViT
+from uvit.dpm_solver_pp import NoiseScheduleVP, DPM_Solver
+
+
+# Constants
+LR = 1e-6
+DECAY = 0.96
+EPOCHS = 50
+BATCHSIZE = 1
+EVALSTEPS = 25
+EVALOUTPUTS = 64
+NORMMEAN = 0.5
+NORMSTDEV = 0.2
+DEVICE = "cuda"
+
+
+# A function to prepare an inference diffusion schedule
+def stable_diffusion_beta_schedule(linear_start=0.00085, linear_end=0.0120, n_timestep=1000):
+    _betas = (
+        torch.linspace(linear_start ** 0.5, linear_end ** 0.5, n_timestep, dtype=torch.float64) ** 2
+    )
+    return _betas.numpy()
+
+
+# Model runner to be passed to DPM Solver
+def model_fn(x, t_continuous):
+    t = t_continuous * len(_betas)
+    _cond = nnet(x, t)
+    return _cond
+
+
+# Prepare data and models
+transform = Compose([
+    ToTensor(),
+    CenterCrop([128, 128]),
+    Normalize(3*[NORMMEAN], 3*[NORMSTDEV])
+])
+dataset = ImageFolder("./data/pokemini", transform=transform)
+dataloader = DataLoader(dataset, batch_size=BATCHSIZE, shuffle=True, num_workers=0)
+model = UViT(img_size=128)
+model.to(DEVICE)
+model.load_state_dict(torch.load(f'./pokemod.pth', weights_only=True))
+model.train()
+score_model = sde.ScoreModel(model, pred='noise_pred', sde=sde.VPSDE())
+optimizer = Adam(model.parameters(), lr=LR)
+lr_scheduler = LambdaLR(optimizer, lr_lambda=lambda epoch: DECAY ** epoch)
+
+
+# Train Model
+best_loss = 9999
+for epoch in range(EPOCHS):
+    avg_loss = 0.
+    num_items = 0
+    for x, _ in dataloader:
+        x = x.to(DEVICE)
+        optimizer.zero_grad()
+        loss = sde.LSimple(score_model, x, pred='noise_pred').mean()
+        loss.backward()
+        optimizer.step()
+        avg_loss += loss.item() * x.shape[0]
+        num_items += x.shape[0]
+    lr_scheduler.step()
+    lr = lr_scheduler.get_last_lr()[0]
+    if avg_loss / num_items < best_loss:
+        torch.save(model.state_dict(), f'./pokemod.pth')
+        best_loss = avg_loss / num_items
+    print(f"Epoch: {epoch+1} [LR: {lr} | Loss: {avg_loss / num_items}]")
+
+
+# Load Saved Model
+nnet = UViT(img_size=128)
+nnet.to(DEVICE)
+model.load_state_dict(torch.load(f'./pokemod.pth', weights_only=True))
+nnet.eval()
+
+
+# Perform Evaluation
+_betas = stable_diffusion_beta_schedule()  # set the noise schedule
+noise_schedule = NoiseScheduleVP(schedule='discrete', betas=torch.tensor(_betas, device=DEVICE).float())
+z_init = torch.randn(EVALOUTPUTS, 3, 128, 128, device=DEVICE)
+dpm_solver = DPM_Solver(model_fn, noise_schedule, predict_x0=True, thresholding=False)
+with torch.no_grad():
+  with torch.cuda.amp.autocast():  # inference with mixed precision
+    samples = dpm_solver.sample(z_init, steps=EVALSTEPS, eps=1. / len(_betas), T=1.)
+denormalize = Compose([
+    Normalize(mean = 3*[0.], std = 3*[1/NORMSTDEV]),
+    Normalize(mean = 3*[-NORMMEAN], std = 3*[1.])
+])
+samples = denormalize(samples)
+save_image(samples, "uvit_output.png")
---
+++ unet/trainunet.py
@@ -0,0 +1,117 @@
+# Libs
+import math, functools
+import numpy as np
+from einops import rearrange
+import matplotlib.pyplot as plt
+
+# PyTorch
+import torch
+import torch.nn as nn
+import torch.multiprocessing
+from torch.optim import Adam
+import torch.nn.functional as F
+from torchvision.datasets import ImageFolder
+from torchvision.utils import make_grid, save_image
+from torch.utils.data import DataLoader, TensorDataset
+from torch.optim.lr_scheduler import MultiplicativeLR, LambdaLR
+from torchvision.transforms import ToTensor, Compose, CenterCrop, Normalize
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+# Extra
+SIGMA = 25.0
+DEVICE = "cuda"
+from diffusionutil import *
+from diffusionmodel import UNet
+marginal_prob_std_fn = functools.partial(marginal_prob_std, sigma=SIGMA, device=DEVICE)
+diffusion_coeff_fn = functools.partial(diffusion_coeff, sigma=SIGMA, device=DEVICE)
+
+
+def train_score_model(score_model, dataset, lr, n_epochs, batch_size, ckpt_name,
+                      marginal_prob_std_fn=marginal_prob_std_fn,
+                      lr_scheduler_fn=lambda epoch: 0.995 ** epoch,
+                      device="cuda"):
+  
+  # Setup Training
+  optimizer = Adam(score_model.parameters(), lr=lr)
+  scheduler = LambdaLR(optimizer, lr_lambda=lr_scheduler_fn)
+  data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+  for epoch in range(n_epochs):
+    score_model.train()
+    avg_loss = 0.
+    num_items = 0
+    for x, y in data_loader:
+      x = x.to(device)
+      y = y.to(device)
+      loss = loss_fn_cond(score_model, x, y, marginal_prob_std_fn)
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      avg_loss += loss.item() * x.shape[0]
+      num_items += x.shape[0]
+    scheduler.step()
+    lr_current = scheduler.get_last_lr()[0]
+    print('{} Average Loss: {:5f} lr {:.1e}'.format(epoch, avg_loss / num_items, lr_current))
+    # Update the checkpoint after each epoch of training.
+    torch.save(score_model.state_dict(), f'ckpt_{ckpt_name}.pth')
+
+
+# Get Dataset
+transform = Compose([
+    ToTensor(),
+    CenterCrop([128, 128]),
+    Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+dataset = ImageFolder("/mnt/data/poke", transform=transform)
+
+# Build Model
+# model = torch.nn.DataParallel(
+#   UNet(marginal_prob_std=marginal_prob_std_fn))
+model = UNet(marginal_prob_std=marginal_prob_std_fn)
+model = model.to(DEVICE)
+
+# Training Step 1
+LR = 1e-3
+EPOCHS = 300
+BATCHSIZE = 32
+DEVICE = 'cuda'
+train_score_model(model, dataset, LR, EPOCHS, BATCHSIZE, "pokemod", device=DEVICE)
+train_score_model(model, dataset, LR, EPOCHS, BATCHSIZE, "pokemod", device=DEVICE) # Do it twice
+
+# Training Step 2
+LR = 5e-4
+EPOCHS = 100
+BATCHSIZE = 1
+train_score_model(model, dataset, LR, EPOCHS, BATCHSIZE, "pokemod", device=DEVICE)
+
+# Training Step 3
+LR = 1e-4
+EPOCHS = 100
+BATCHSIZE = 1
+train_score_model(model, dataset, LR, EPOCHS, BATCHSIZE, "pokemod", device=DEVICE)
+
+# Training Step 4
+LR = 1e-6
+EPOCHS = 200
+BATCHSIZE = 1
+train_score_model(model, dataset, LR, EPOCHS, BATCHSIZE, "pokemod", device=DEVICE,
+                  lr_scheduler_fn=lambda epoch: 1)
+
+# Run Sample
+model = UNet(marginal_prob_std=marginal_prob_std_fn)
+model.load_state_dict(torch.load(f'ckpt_pokemod.pth', weights_only=True))
+model.to(DEVICE)
+sample_batch_size = 64
+num_steps = 250
+sampler = Euler_Maruyama_sampler
+samples = sampler(model,
+        marginal_prob_std_fn,
+        diffusion_coeff_fn,
+        sample_batch_size,
+        x_shape=(3, 128, 128),
+        num_steps=num_steps,
+        device=DEVICE,
+        y=torch.zeros([64], dtype=torch.int32).to(DEVICE))
+denormalize = Normalize([-0.485/0.229, -0.456/0.224, -0.406/0.225],
+                        [1/0.229, 1/0.224, 1/0.225])
+samples = denormalize(samples).clamp(0.0, 1.0)
+save_image(samples, "test.png")
